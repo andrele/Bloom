5b5f6884da4a7df157c0c1e63a2199dfd8b0f7cc
Prachi Pendse <prachirp@hotmail.com>
2013-09-29 08:38:11 -0400

    Added javascipt file to autocomplete input elements. Also moved js files into corresponding js folder.

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,1 +1,33 @@
+// An example Backbone application contributed by
+// [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses a simple
+// [LocalStorage adapter](backbone-localstorage.html)
+// to persist Backbone models within your browser.
+
+// Load the application once the DOM is ready, using `jQuery.ready`:
+function transitionPages()
+{
+	console.log('transition');
+}
+
+$('#goal').typeahead([
+{
+	name: 'availableGoals',
+	local: [ "Run a marathon", "Learn to play the ukelele", "Plan a wedding", "Quit smoking cigarettes", "Learn to speak Chinese this year"]
+}
+]);
+
+$('#goal').keypress(function (e)
+{
+  if (e.which == 13)
+  {
+    transitionPages();
+  }
+});
+
+$( "#Start" ).click(function()
+{
+  transitionPages();
+ 
+});
+
 
