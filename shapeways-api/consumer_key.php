1cf6940695fc8b8203dfd1bde98ed0fcde0c1ae6
Jeff Crossman <jeff@jeffcrossman.com>
2013-09-28 20:49:03 -0400

    Added App Keys

--- shapeways-api/consumer_key.php
+++ shapeways-api/consumer_key.php
@@ -1,5 +1,5 @@
 <?php
 
-$consumer_key    = ""; # CHANGEME
-$consumer_secret = ""; # CHANGEME
+$consumer_key    = "26ffd992cee20fb919d1003770a8eb01764ad9d0"; # CHANGEME
+$consumer_secret = "2d40bde2beefce0f6e25817b8e0aeed5e40b35ac"; # CHANGEME
 
